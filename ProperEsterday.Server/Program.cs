7ecf80d587bd448d4272d0ad8289dbad6a16943e
Vilayat.Khan <vilayatk96@outlook.com>
2024-08-31 21:59:43 +0530

    add checkbox and support forecasts database

--- ProperEsterday.Server/Program.cs
+++ ProperEsterday.Server/Program.cs
@@ -24,8 +24,19 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+var forecastsDatabase = Enumerable.Range(1, 7).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+
 app.MapGet("/weatherforecast/{id}", (int id) =>
 {
+    if (id == -1) return forecastsDatabase;
+
     var forecast = Enumerable.Range(1, id).Select(index =>
         new WeatherForecast
         (
